b4d09fb4deda5b5f0eb17cc845dd76df6f802ebe
lcoburn <lukecoburn@gmail.com>
2023-04-05 20:57:06 +0200

    fixed static fieles missing bracket

--- easter_hunt/settings.py
+++ easter_hunt/settings.py
@@ -123,7 +123,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'easter_hunt/static'),
-django_heroku.settings(locals())
+    django_heroku.settings(locals()),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
