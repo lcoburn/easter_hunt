a01e43df2a6a549fcf7136edf0bae85fc6524b5f
lcoburn <lukecoburn@gmail.com>
2023-04-06 10:05:09 +0200

    add a bunch of htmls

--- pages/views.py
+++ pages/views.py
@@ -10,4 +10,28 @@ def map(request):
  return render(request, 'pages/map.html')
 def help(request):
  return render(request, 'pages/help.html')
+def clue1(request):
+ return render(request, 'pages/clue1.html')
+def clue2(request):
+ return render(request, 'pages/clue2.html')
+def clue3(request):
+ return render(request, 'pages/clue3.html')
+def clue4(request):
+ return render(request, 'pages/clue4.html')
+def clue5(request):
+ return render(request, 'pages/clue5.html')
+def clue6(request):
+ return render(request, 'pages/clue6.html')
+def clue7(request):
+ return render(request, 'pages/clue7.html')
+def clue8(request):
+ return render(request, 'pages/clue8.html')
+def clue9(request):
+ return render(request, 'pages/clue9.html')
+def clue10(request):
+ return render(request, 'pages/clue10.html')
+def clue11(request):
+ return render(request, 'pages/clue11.html')
+def clue12(request):
+ return render(request, 'pages/clue12.html')
 
