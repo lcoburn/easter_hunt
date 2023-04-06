9178ab3942dcfd5caf1003cbd3e172d03ccd1c9e
lcoburn <lukecoburn@gmail.com>
2023-04-06 09:36:04 +0200

    added map page

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('introduction', views.introduction,name='introduction'),
+    path('map', views.map,name='map'),
     path('help', views.help,name='help'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,8 @@ def home(request):
  return render(request, 'pages/home.html')
 def introduction(request):
  return render(request, 'pages/introduction.html')
+def map(request):
+ return render(request, 'pages/map.html')
 def help(request):
  return render(request, 'pages/help.html')
 
